8ab3b63364aa9938da405743dbd7b39eee3f64ed
Conor McGrath <cmcgrat3@nd.edu>
2021-02-24 18:31:06 -0500

    Scraped KenPom

---
+++ KenPom_Scrape.R
@@ -0,0 +1,103 @@
+# Scrape data from KenPom
+
+#http://kenpom.com/
+library('data.table')
+library('pbapply')
+library('XML')
+library('RCurl')
+library('stringdist')
+library('stringi')
+library(httr)
+rm(list=ls(all=TRUE))
+gc(reset=TRUE)
+set.seed(8865)
+
+#Load Spellings
+spell <- fread('data/MTeamSpellings.csv')
+
+#Manually add some spellings
+new1 <- copy(spell[TeamNameSpelling == 'citadel',])
+new1[,TeamNameSpelling := 'the citadel']
+
+new2 <- copy(spell[TeamNameSpelling == 'fort wayne(ipfw)',])
+new2[,TeamNameSpelling := 'fort wayne']
+
+new3 <- copy(spell[TeamNameSpelling == 'fort wayne(ipfw)',])
+new3[,TeamNameSpelling := 'ft wayne']
+
+spell <- rbindlist(list(
+  spell,
+  new1,
+  new2,
+  new3
+))
+
+#Scrape ratings
+dat_list <- pblapply(2002:2021, function(x){
+  Sys.sleep(1)
+  page <- GET(paste0('https://kenpom.com/index.php?y=', x))
+  out <- read_html(page)%>%
+    html_table(fill=TRUE)%>%
+    `[[`(1)
+  data.table(
+    Season = x,
+    out
+  )
+})
+
+#Combine and cleanup dataframe
+dat <- rbindlist(dat_list)
+remove <- paste0('V', c(7, 9, 11, 13))
+set(dat, j=remove, value=NULL)
+setnames(dat, c(
+  'Season',
+  'Rank',
+  'Team',
+  'Conf',
+  'W-L',
+  'AdjEM',
+  'AdjO',
+  'AdjD',
+  'AdjT',
+  'Luck',
+  'schedule_AdjEM',
+  "remove1",
+  'schedule_OppO',
+  "remove2",
+  'schedule_OppD',
+  "remove3",
+  'conf_AdjEM',
+  "remove4"
+))
+dat <- dat %>%
+  select(-remove1, -remove2, -remove3, -remove4)
+dat <- dat[!(is.na(AdjEM) | is.na(Rank) | is.na(schedule_AdjEM)),]
+for(var in names(dat)[6:ncol(dat)]){
+  set(dat, j=var, value=as.numeric(dat[[var]]))
+}
+
+#Match to spellings
+cleanup <- function(x){
+  x <- gsub('[[:digit:]]+', ' ', x)
+  x <- gsub('[[:space:]]+', ' ', x)
+  x <- stringi::stri_trim_both(x)
+  x <- stringi::stri_trans_tolower(x)
+  return(x)
+}
+dat[,Team := cleanup(Team)]
+spell[,TeamNameSpelling := cleanup(TeamNameSpelling)]
+
+matches <- amatch(dat[['Team']], spell[['TeamNameSpelling']], method='cosine')
+
+dat[,TeamID := spell[matches, 'TeamID']]
+dat[,alternative_spelling := spell[matches, 'TeamNameSpelling']]
+dat[is.na(TeamID),]
+dat[,match_rating := 1-stringdist(Team, alternative_spelling, method='cosine')]
+dat[Team != alternative_spelling,][order(match_rating),unique(data.table(Team, alternative_spelling, match_rating))]
+
+naMask <- is.na(dat$AdjEM)
+
+dat <- dat[naMask ==F,]
+#Save
+PomeryRatings <- dat
+write.csv(PomeryRatings, 'data/PomeryRatings.csv', row.names=FALSE)
--- March_Madness_Predictions.R
+++ March_Madness_Predictions.R
@@ -62,5 +62,22 @@ table <- html_nodes(website, xpath = "/html/body/div[1]/div/div/div[3]/div[1]")
 seeds <- html_table(xml_child(table[[1]]))
 
 
+library(stringr)
+library(plyr)
+seeds <- seeds %>%
+  mutate_all(funs(str_replace(.,"%","")))%>%
+  lapply(.,as.numeric)%>%
+  as.data.frame()
+
+seeds[,-1] <- seeds[,-1]/100
+
+conditional_probs <- seeds %>%
+  mutate(FirstRound = Second.Round)%>%
+  mutate(SecondRound = round(Sweet.16/Second.Round,3))%>%
+  mutate(Sweet16 = round(Elite.Eight/Sweet.16,3))%>%
+  mutate(EliteEight = round(Final.4/Elite.Eight,3))%>%
+  mutate(FinalFour = round(Title.Game/Final.4,3))%>%
+  mutate(TitleGame = round(National.Champ..Win./Title.Game,3))%>%
+  select(Seed,FirstRound, SecondRound, Sweet16, EliteEight, FinalFour, TitleGame)
 
 

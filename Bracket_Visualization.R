2966947055a4057b61a592c9932d8611e1354e7c
Conor McGrath <cmcgrat3@nd.edu>
2021-03-02 20:43:20 -0500

    Upload DayNum Converter csv from Kaggle

--- Bracket_Visualization.R
+++ Bracket_Visualization.R
@@ -2,7 +2,7 @@
 # This code might be useful after we make our predictions. This converts predictions to visual bracket
 devtools::install_github('zachmayer/kaggleNCAA')
 library('kaggleNCAA')
-dat <- parseBracket('seed_benchmark_men.csv', w=0)  # w=0 for men
+dat <- parseBracket('data/preds_to_send.csv', w=0)  # w=0 for men
 sim <- simTourney(dat, 100, progress=TRUE, w=0)  # w=0 for men
 bracket <- extractBracket(sim)
 printableBracket(bracket)
